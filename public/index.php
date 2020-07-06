6f18f2ddcbaff96b21a99faec220844055e28076
Christer Edvartsen <christer.edvartsen@nav.no>
2020-07-06 12:34:53 +0200

    Initial commit of main project source

---
+++ public/index.php
@@ -0,0 +1,98 @@
+<?php declare(strict_types=1);
+namespace Nais\Device\Approval;
+
+use NAVIT\AzureAd\ApiClient;
+use DI\Container;
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+use Nais\Device\Approval\Controllers\IndexController;
+use Nais\Device\Approval\Controllers\MembershipController;
+use Nais\Device\Approval\Controllers\SamlController;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Throwable;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * Get env var as string
+ *
+ * @param string $key
+ * @return string
+ */
+function env(string $key) : string {
+    return trim((string) getenv($key));
+}
+
+define('DEBUG', '1' === env('DEBUG'));
+
+// Create and populate container
+$container = new Container();
+$container->set(Twig::class, fn() : Twig => Twig::create(__DIR__ . '/../templates'));
+$container->set(Session::class, fn() : Session => (new Session())->start() );
+$container->set(ApiClient::class, fn() => new ApiClient(env('AAD_CLIENT_ID'), env('AAD_CLIENT_SECRET'), env('DOMAIN')));
+$container->set(SamlResponseValidator::class, fn() => new SamlResponseValidator(env('SAML_CERT')));
+$container->set(IndexController::class, function(ContainerInterface $c) : IndexController {
+    /** @var ApiClient */
+    $apiClient = $c->get(ApiClient::class);
+
+    /** @var Twig */
+    $twig = $c->get(Twig::class);
+
+    /** @var Session */
+    $session = $c->get(Session::class);
+
+    return  new IndexController($apiClient, $twig, $session, env('LOGIN_URL'), env('ISSUER_ENTITY_ID'), env('ACCESS_GROUP'));
+});
+$container->set(SamlController::class, function(ContainerInterface $c) : SamlController {
+    /** @var Session */
+    $session = $c->get(Session::class);
+
+    /** @var SamlResponseValidator */
+    $validator = $c->get(SamlResponseValidator::class);
+
+    return new SamlController($session, $validator, env('LOGOUT_URL'));
+});
+$container->set(MembershipController::class, function(ContainerInterface $c) : MembershipController {
+    /** @var Session */
+    $session = $c->get(Session::class);
+
+    /** @var ApiClient */
+    $apiClient = $c->get(ApiClient::class);
+
+    return new MembershipController($session, $apiClient, env('ACCESS_GROUP'));
+});
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+
+// Register middleware
+$app->addBodyParsingMiddleware();
+$app->add(TwigMiddleware::createFromContainer($app, Twig::class));
+$app->add(new Middleware\EnvironmentValidation(getenv()));
+$app
+    ->addErrorMiddleware(DEBUG, true, true)
+    ->setDefaultErrorHandler(function(Request $request, Throwable $exception, bool $displayErrorDetails) use ($app) {
+        /** @var ContainerInterface */
+        $container = $app->getContainer();
+
+        /** @var Twig */
+        $twig = $container->get(Twig::class);
+
+        return $twig->render($app->getResponseFactory()->createResponse(500), 'error.html', [
+            'errorMessage' => $displayErrorDetails ? $exception->getMessage() : 'An error occurred',
+        ]);
+    });
+
+// Routes
+$app->get('/',                  IndexController::class . ':index');
+$app->post('/toggleMembership', MembershipController::class . ':toggle');
+$app->post('/saml/acs',         SamlController::class . ':acs');
+$app->get('/saml/logout',       SamlController::class . ':logout');
+$app->get('/isAlive',           fn(Request $request, Response $response) : Response => $response);
+$app->get('/isReady',           fn(Request $request, Response $response) : Response => $response);
+
+// Run the app
+$app->run();
---
+++ src/Controllers/MembershipController.php
@@ -0,0 +1,67 @@
+<?php declare(strict_types=1);
+namespace Nais\Device\Approval\Controllers;
+
+use Nais\Device\Approval\Session;
+use NAVIT\AzureAd\ApiClient;
+use NAVIT\AzureAd\Models\Group;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use RuntimeException;
+
+class MembershipController {
+    private Session $session;
+    private ApiClient $apiClient;
+    private string $accessGroup;
+
+    public function __construct(Session $session, ApiClient $apiClient, string $accessGroup) {
+        $this->session     = $session;
+        $this->apiClient   = $apiClient;
+        $this->accessGroup = $accessGroup;
+    }
+
+    public function toggle(Request $request, Response $response) : Response {
+        $response         = $response->withHeader('Content-Type', 'application/json');
+        $user             = $this->session->getUser();
+        $sessionToken     = $this->session->getPostToken();
+
+        /** @var array{token: ?string} */
+        $post             = $request->getParsedBody();
+        $tokenFromRequest = $post['token'] ?? null;
+
+        if (null === $user) {
+            $response->getBody()->write((string) json_encode(['error' => 'Invalid session']));
+            return $response->withStatus(400);
+        } else if (null === $sessionToken) {
+            $response->getBody()->write((string) json_encode(['error' => 'Missing session token']));
+            return $response->withStatus(400);
+        } else if ($sessionToken !== $tokenFromRequest) {
+            $response->getBody()->write((string) json_encode(['error' => 'Incorrect session token']));
+            return $response->withStatus(400);
+        }
+
+        try {
+            $groups = array_filter($this->apiClient->getUserGroups($user->getObjectId()), function(Group $group) : bool {
+                return $group->getId() === $this->accessGroup;
+            });
+        } catch (RuntimeException $e) {
+            $response->getBody()->write((string) json_encode(['error' => 'Unable to fetch user groups']));
+            return $response->withStatus(400);
+        }
+
+        $hasAccepted = 0 !== count($groups);
+
+        try {
+            if ($hasAccepted) {
+                $this->apiClient->removeUserFromGroup($user->getObjectId(), $this->accessGroup);
+            } else {
+                $this->apiClient->addUserToGroup($user->getObjectId(), $this->accessGroup);
+            }
+        } catch (RuntimeException $e) {
+            $response->getBody()->write((string) json_encode(['error' => 'Unable to toggle group membership']));
+            return $response->withStatus(400);
+        }
+
+        $response->getBody()->write((string) json_encode(['success' => true, 'hasAccepted' => !$hasAccepted]));
+        return $response->withStatus(200);
+    }
+}
