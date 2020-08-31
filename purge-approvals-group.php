8d50c484b2cff6383a4f57ce21aa2261293cae41
Christer Edvartsen <christer.edvartsen@nav.no>
2020-08-31 15:01:44 +0200

    Add script that purges the group members

---
+++ purge-approvals-group.php
@@ -0,0 +1,15 @@
+<?php
+require 'vendor/autoload.php';
+
+foreach (['AAD_CLIENT_ID', 'AAD_CLIENT_SECRET', 'DOMAIN', 'ACCESS_GROUP'] as $required) {
+    if (empty(getenv($required))) {
+        echo sprintf('Missing required environment variable: %s', $required) . PHP_EOL;
+        exit(1);
+    }
+}
+
+(new \NAVIT\AzureAd\ApiClient(
+    getenv('AAD_CLIENT_ID'),
+    getenv('AAD_CLIENT_SECRET'),
+    getenv('DOMAIN'),
+))->emptyGroup(getenv('ACCESS_GROUP'));
